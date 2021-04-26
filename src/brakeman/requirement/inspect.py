ce180cbe6d43d9245c1b94acb6007fbd60538b39
Shaoyong Feng <feng.sha@husky.neu.edu>
2021-04-26 03:20:32 -0400

    find vul based on safedb

--- src/brakeman/requirement/inspect.py
+++ src/brakeman/requirement/inspect.py
@@ -1,23 +1,62 @@
-#from safety_db import INSECURE, INSECURE_FULL
-from typing import Any
+from typing import Any, Generator
 
-from packaging.version import Version, parse
-import requirements
-import operator
-from .vrange import VRange
-
-
-def get_requirement_packages(filename: str = 'requirements.txt') -> list:
-    with open(filename, 'r') as fd:
-        return list(requirements.parse(fd))
+from packaging.version import Version
+from packaging.specifiers import SpecifierSet
+from packaging.requirements import Requirement
+from safety_db import INSECURE, INSECURE_FULL
 
 
-class Requirement(object):
-    def __init__(self, req: Any) -> None:
-        """
-        Convert from Requirement object in "requirements" package
-        """
-        self.line = req.line
-        self.name = req.name
-        self.path = req.path
-        self.raw = req
+def is_comment(line: str) -> bool:
+    return str(line).strip().startswith('#')
+
+
+def versions() -> Generator[str]:
+    for v in range(100):
+        yield "{0.1f}".format(v / 10)
+
+
+def specs_intersect(spec1: SpecifierSet, spec2: SpecifierSet) -> bool:
+    """
+    Check if two specSet intersect
+    packaging does not provide the functionality, and no way to get boundary
+    a simple method to check
+    otherwise use the VRange method
+    """
+    for v in versions():
+        if v in spec1 and v in spec2:
+            return True
+
+    return False
+
+
+def spec_intersect_list(spec1: SpecifierSet, specs: list[SpecifierSet]) -> bool:
+    """
+    Check spec1 intersect with any of specs
+    """
+    for spec2 in specs:
+        if specs_intersect(spec1, spec2):
+            return True
+    return False
+
+
+def parse_file(file: str= 'requirement.txt') -> Generator[int, Requirement]:
+    with open(file, 'r') as fp:
+        for cnt, line in enumerate(fp):
+            line = line.strip()
+            if not is_comment(line) and len(line) > 0:
+                yield cnt, Requirement(line)
+
+
+def find_vulnerabilities(package: Requirement) -> list[dict]:
+    name, spec = package.name, package.specifier
+    if name not in INSECURE_FULL:
+        return []
+
+    vulnerabilities = INSECURE_FULL[name]
+    exist_vuls = list()
+    for vul in vulnerabilities:
+        specs = [SpecifierSet(s) for s in vul["specs"]]
+        if spec_intersect_list(spec, specs):
+            exist_vuls.append(vul)
+
+    return exist_vuls
--- tests/requirement/test_inspect.py
+++ tests/requirement/test_inspect.py
@@ -7,7 +7,7 @@ from brakeman.requirement import inspect
 
 REQFILE_DIR = os.path.join(os.path.dirname(__file__), 'reqfiles')
 
+
 def test_inspect():
-    ls = inspect.get_requirement_packages(REQFILE_DIR+'/req1.txt')
-    for l in ls:
-        print(l.name, l.specs)
+    res = list(inspect.parse_file(REQFILE_DIR+'/req1.txt'))
+    print(res)
