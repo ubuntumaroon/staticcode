8347df9e92c1f3dcc58e1eaa56998eb09965ef0e
Shaoyong Feng <feng.sha@husky.neu.edu>
2021-04-27 18:47:36 -0400

    change output

--- tests/requirement/test_inspect.py
+++ tests/requirement/test_inspect.py
@@ -9,4 +9,5 @@ REQFILE_DIR = os.path.join(os.path.dirname(__file__), 'reqfiles')
 
 def test_inspect():
     res = inspect.ReqInspector(REQFILE_DIR + '/req1.txt')
+    res.print()
     assert res.all_vuls_str() is not ""
