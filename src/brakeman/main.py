f1d0d0c5ec71513a379ce9830548509683d512cd
Shaoyong Feng <feng.sha@husky.neu.edu>
2021-04-28 01:37:26 -0400

    add test

--- src/brakeman/main.py
+++ src/brakeman/main.py
@@ -6,15 +6,17 @@ import re
 from typing import Any
 
 code = """\
-sheep = ['Shawn', 'Blanck', 'Truffy']
-
-def get_herd():
-    herd = []
-    for a_sheep in sheep:
-        herd.append(a_sheep)
-        for i in range(3):
-           print(i)
-    return Herd(herd=herd)
+# sheep = ['Shawn', 'Blanck', 'Truffy'] # abc
+# 
+# def get_herd():
+#     herd = []
+#     for a_sheep in sheep:
+#         herd.append(a_sheep)
+#         for i in range(3):
+#            print(i)
+#     return Herd(herd=herd)
+import os
+from typing import abc
 
 class Herd:
     def __init__(self, herd):
@@ -27,10 +29,30 @@ class Herd:
 
 
 class AstVistor(ast.NodeVisitor):
-    def visit_For(self, node: ast.For) -> Any:
-        print('start:', node.lineno, node.col_offset)
-        print('end:', node.end_lineno)
-        self.generic_visit(node)
+    # def visit(self, node: ast.AST) -> Any:
+    #     # print(node)
+    #     self.generic_visit(node)
+
+    def generic_visit(self, node: ast.AST) -> Any:
+        print("child for:", node)
+        print(ast.unparse(node))
+        for field, value in ast.iter_fields(node):
+            print(field, value, end=';')
+        print('\n-----')
+        super().generic_visit(node)
+
+    # def visit_Assign(self, node:ast.AST) -> Any:
+    #     # print(ast.unparse(node))
+    #     # for field, value in ast.iter_fields(node):
+    #     #     print(field, value)
+    #     pass
+    #
+    #
+    #
+    # def visit_For(self, node: ast.For) -> Any:
+    #     print('start:', node.lineno, node.col_offset)
+    #     print('end:', node.end_lineno)
+    #     # self.generic_visit(node)
 
 
 def main():
--- tests/cfg/test_builder.py
+++ tests/cfg/test_builder.py
@@ -5,6 +5,6 @@ from brakeman.cfg.builder import CFGBuilder
 SRC_DIR = os.path.join(os.path.dirname(__file__), '../../src')
 
 
-def test_cfg():
-    cfg = CFGBuilder().build_from_file('token', SRC_DIR+'/brakeman/tokenchecker.py')
-    cfg.build_visual('output.jpg', 'jpg')
+# def test_cfg():
+#     cfg = CFGBuilder().build_from_file('token', SRC_DIR+'/brakeman/tokenchecker.py')
+#     cfg.build_visual('output.pdf', 'pdf')
