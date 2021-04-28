01e6be79ba2927bb20a41780f19f7669d168a807
Shaoyong Feng <feng.sha@husky.neu.edu>
2021-04-27 20:38:36 -0400

    add test

---
+++ tests/cfg/test_builder.py
@@ -0,0 +1,10 @@
+import os
+from brakeman.cfg.builder import CFGBuilder
+
+
+SRC_DIR = os.path.join(os.path.dirname(__file__), '../../src')
+
+
+def test_cfg():
+    cfg = CFGBuilder().build_from_file('token', SRC_DIR+'/brakeman/tokenchecker.py')
+    cfg.build_visual('output.jpg', 'jpg')
